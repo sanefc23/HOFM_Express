e44cd1bce0ca6cf17e631ca6d3b4ace50e9d23da
sanefc23 <san.efc@gmail.com>
2020-02-19 09:08:15 -0300

    se agrega script de población de datos

---
+++ docs/Database/HofM_script_insert_data.sql
@@ -0,0 +1,115 @@
+INSERT INTO house_of_music_db.albums (
+	title,
+	artists_id,
+	description,
+	front_cover,
+	back_cover,
+	rating,
+	release_date,
+	genre_id
+	)
+	VALUES
+	(
+	'Norman Fucking Rockwell',
+	1,
+	'Norman Fucking Rockwell, lanzado el 30 de agosto de 2019, es el sexto álbum de estudio de la cantante estadounidense Lana Del Rey. Fue co-producido co',
+	'/images/albums/lana.jpg',
+	'/images/albums/lana2.jpeg',
+	'5',
+	'2019-08-30',
+	1	
+	),
+	(
+	'Masseduction',
+	2,
+	'Masseduction es el 5to album estudio de St. Vincent, producido por Jack Antonoff. El disco tiene los tintes característicos de Annie Clark: pop y glamrock.',
+	'/images/albums/masseduction.jpg',
+	'/images/albums/masseductionback.jpg',
+	4.5,
+	'2017-10-02',
+	1
+	),
+	(
+	'Hot Pink',
+	8,
+	'Hot Pink is the second studio album by American rapper and singer Doja Cat. The album was released on November 7, 2019, by Kemosabe. It features the singles "Juicy", featuring Tyga, "Bottom Bitch" and "Rules". The album features guest appearances from Smino, Tyga and Gucci Mane.',
+	'/images/albums/front_cover-1581980148876.jpeg',
+	'/images/albums/back_cover-1581980148877.jpeg',
+	4,
+	'2019-11-07',
+	1
+	),
+	(
+	'Yo x Ti, Tu x Mi',
+	4,
+	'Nuevo single de la cantante española junto con Ozuna. El tema mezcla el pop con el reggaetón, y el flamenco.',
+	'/images/albums/front_cover-1581981266259.jpg',
+	'/images/albums/back_cover-1581981266261.jpg',
+	4,
+	'2019-08-15',
+	12
+	),(
+	'Thank U, Next',
+	10,
+	'Quinto álbum de estudio de la cantante y actriz estadounidense Ariana Grande, lanzado el 8 de febrero de 2019.',
+	'/images/albums/front_cover-1581981709132.jpg',
+	'/images/albums/back_cover-1581981709134.jpg',
+	5,
+	'2019-02-08',
+	1
+	),
+	(
+	'Oasis',
+	59,
+	'Oasis es un álbum de estudio colaborativo entre el cantante puertorriqueño Bad Bunny y el cantante colombiano J Balvin. El álbum abarca desde el Reggaeton al Latin Trap e incluye leves pinceladas de folclore latino y Jazz.',
+	'/images/albums/front_cover-1581983495561.jpeg',
+	'/images/albums/back_cover-1581983495565.jpg',
+	4,
+	'2019-06-28',
+	12
+	),(
+	'Halcyon Days',
+	53,
+	'Halcyon Days es la reedición del segundo álbum de estudio de la cantante y artista británica Ellie Goulding: Halcyon (2012), fue lanzado el 26 de agosto de 2013 en Reino Unido y el 27 de agosto de 2013 en Estados Unidos.',
+	'/images/albums/front_cover-1581984728973.jpg',
+	'/images/albums/back_cover-1581984728974.jpg',
+	3,
+	'2013-08-26',
+	1
+	), (
+	'When We All Fall Asleep, Where Do We Go?',
+	'46',
+	'When We All Fall Asleep, Where Do We Go? es el álbum de estudio debut de la cantante estadounidense Billie Eilish. Está producido por su hermano Finneas OConnell y fue publicado el 29 de marzo de 2019. El álbum combina géneros como el electropop, influencias de R&B y el pop.',
+	'/images/albums/front_cover-1581985863185.jpg',
+	'/images/albums/back_cover-1581985863189.jpg',
+	4,
+	'2019-03-29',
+	1
+	),(
+	'El Mal Querer',
+	4,
+	'El mal querer es el segundo trabajo álbum de estudio de la cantante española Rosalía, publicado el 2 de noviembre de 2018 a través de Sony Music.​​ Está producido conjuntamente por ella misma y El Guincho.',
+	'/images/albums/front_cover-1582071796264.jpg',
+	'/images/albums/back_cover-1582071796267.jpg',
+	5,
+	'2018-11-02',
+	1
+	),(
+	'The Now Now',
+	60,
+	'The Now Now es el sexto álbum de estudio de la banda virtual británica Gorillaz. Fue lanzado el 29 de junio de 2018, bajo el sello discográfico de Parlophone y Warner Bros.',
+	'/images/albums/front_cover-1582079790936.jpg',
+	'/images/albums/back_cover-1582079790938.jpg',
+	3,
+	'2018-06-29',
+	8
+	), (
+	'The Thrill Of It All',
+	26,
+	'The Thrill Of It All es el segundo álbum de estudio cantautor británico Sam Smith.1​ Se lanzó el 3 de noviembre de 2017 a través de Capitol Records.',
+	'/images/albums/front_cover-1582081658292.jpg',
+	'/images/albums/back_cover-1582081658296.jpg',
+	4,
+	'2017-11-03',
+	1
+	)
